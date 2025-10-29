70e853bc8afe2539bbe6a0be9869c3857c1dd4ce
Yaroslav Homziak <ywgk0102@gmail.com>
2025-10-29 19:10:34 +0200

    feat (labels) : add custom labels to reuse them on project. Add UiColor extension

---
+++ OnboardingApp/Core/Extensions/UIColor+Ext.swift
@@ -0,0 +1,11 @@
+//
+//  UIColor+Ext.swift
+//  OnboardingApp
+//
+//  Created by Yaroslav Homziak on 29.10.2025.
+//
+
+import UIKit
+extension UIColor {
+    static let primaryText = UIColor(named: "TextPrimary") ?? .label
+}
---
+++ OnboardingApp/Core/UIComponents/Labels/H1Label.swift
@@ -0,0 +1,17 @@
+//
+//  TitleLabel.swift
+//  OnboardingApp
+//
+//  Created by Yaroslav Homziak on 29.10.2025.
+//
+
+import UIKit
+
+class H1Label: BaseLabel {
+    override func configure() {
+        super.configure()
+        font = .systemFont(ofSize: 26, weight: .bold)
+        textColor = .primaryText
+        numberOfLines = 0
+    }
+}
