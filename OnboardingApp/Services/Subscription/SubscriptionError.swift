261990e1d884dfc53ff49804c0424c7ed7e119bd
Yaroslav Homziak <ywgk0102@gmail.com>
2025-10-30 18:28:17 +0200

    feat (storeKit) : add local product and subscription service

---
+++ OnboardingApp/Services/Subscription/SubscriptionError.swift
@@ -0,0 +1,13 @@
+//
+//  SubscriptionError.swift
+//  OnboardingApp
+//
+//  Created by Yaroslav Homziak on 30.10.2025.
+//
+
+import Foundation
+enum SubscriptionError: Error {
+    case productNotFound
+    case purchaseFailed
+    case vericationFailed
+}
